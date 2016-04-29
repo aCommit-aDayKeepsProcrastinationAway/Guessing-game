cc9ab65d61bd0b8e1734429dd73117f0a4620ada
someguy147 <someguy147@users.noreply.github.com>
2016-04-29 16:03:36 +0300

    Completely had to rewrite to include replay function, game engine and replay feature transfered to void functions

--- Guessing game.cpp
+++ Guessing game.cpp
@@ -4,9 +4,18 @@
 
 using namespace std;
 
-int main ()
+void game ();
+void gamereset();
+
+int main ()			// Had to write everything in functions because main can't call itself
 {
-    bool gamefinished = false;
+	game();
+  	return 0;
+}
+
+void game ()
+{
+	bool gamefinished = false;
     int rand_n, guess;
     srand(time(0));
     rand_n = rand() % 101;
@@ -18,6 +27,18 @@ int main ()
         else if (guess > rand_n) cout << "Nope, try taking a lower number" << endl;
         else if (guess < rand_n) cout << "Wrong, try taking a higher number" << endl;
     }
-    cout << "Good job, you guessed it. The number is : " << rand_n << " .";
-    return 0;
+    gamereset();		// Call to function which asks if player wants to play again
+}
+
+void gamereset()
+{
+	string request;
+	cout << "Do you want to play again? (y/n)" << endl; cin >> request;
+	if (request == "y") return game();		//Calls game function if players want to play again, reason why I had to rewrite this
+	else if (request == "n");				//and couldn't use the main function for game.
+	else 
+	{
+		cout << "Incorrect answer, please use y (for yes) or n (for no)." << endl;
+		gamereset();						// Calls to ask the same question again, reason why I had to use two different functions
+	}   									// apart from main.
 }
